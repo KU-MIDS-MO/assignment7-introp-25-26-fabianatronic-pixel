a0c40e8d0396e81abbba492861d3e6d43c68ea41
fabianatronic-pixel <fabiana.tronic@gmail.com>
2025-12-23 01:28:55 +0100

    Add files via upload

--- build_pipeline.py
+++ build_pipeline.py
@@ -1,4 +1,25 @@
 def build_pipeline(operation_names):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    
+    valid_ops = {"add_one", "double", "triple", "square"}
+    
+    for op in operation_names:
+        if op not in valid_ops:
+            raise KeyError (op)
+            
+    def pipeline(x):
+        result = x
+        
+        for op in operation_names:
+            if op == "add_one":
+                result = result + 1
+            elif op == "double":
+                result = result * 2 
+            elif op == "triple":
+                result = result *3
+            elif op == "square":
+                result = result **2
+            
+                
+        return result
+    
+    return pipeline
--- make_account.py
+++ make_account.py
@@ -1,4 +1,25 @@
 def make_account(initial_balance):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if not isinstance (initial_balance, (int,float)) or initial_balance < 0:
+        raise ValueError ("Initial balance must be a non-negativve number")
+        
+    balance = initial_balance
+    
+    def deposit (amount):
+        nonlocal balance
+        if not isinstance (amount, (int,float)) or amount <= 0:
+            raise ValueError ("Deposit amount must be positive")
+        balance += amount
+        return balance
+    
+    def withdraw (amount):
+        nonlocal balance
+        if not isinstance (amount, (int,float)) or amount <=0:
+           raise ValueError ("Withdrawal amount must be positive")
+        if amount > balance:
+           raise ValueError ("Insufficient funds")
+        balance -= amount
+        return balance
+    
+    return deposit, withdraw
+
+    
